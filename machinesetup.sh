8bcb3b24d730d003f85ef2f9b92dc46a9b2aff8d
Robert Reichel <rjreiche@ncsu.edu>
2017-01-02 12:39:57 -0500

    Update machinesetup.sh

--- machinesetup.sh
+++ machinesetup.sh
@@ -3,8 +3,15 @@ uid=$(id -u)
 ## Check for root
 [ $uid -ne 0 ] && { echo "Run this as root."; exit 1; }
 xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install glib binutils cmake openssl python
+curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.5.tar.bz2
+tar xf MacPorts-2.3.5.tar.bz2
+cd MacPorts-2.3.5/
+./configure
+make
+sudo make install
+cd ..
+port install glib binutils cmake openssl python
+# Setup my shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Change the oh-my-zsh theme in ~/.zshrc (usually use agnoster)"
 sed 's/.*ZSH_THEME=.*/ZSH_THEME="agnoster"/' ~/.zshrc
