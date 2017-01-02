8733abda367a888819d9e1df12f5c3368893405a
Robert Reichel <rjreiche@ncsu.edu>
2017-01-02 13:24:12 -0500

    Update machinesetup.sh

--- machinesetup.sh
+++ machinesetup.sh
@@ -1,22 +1,27 @@
 #machinesetup.sh
+MACPORT_VERSION="2.3.5"
+MACPORT_FILE="MacPorts-" + $MACPORT_VERSION 
 uid=$(id -u)
 ## Check for root
 [ $uid -ne 0 ] && { echo "Run this as root."; exit 1; }
+# Install xcode tools
 xcode-select --install
-curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.5.tar.bz2
-tar xf MacPorts-2.3.5.tar.bz2
-cd MacPorts-2.3.5/
+# Install MacPorts
+curl -O https://distfiles.macports.org/MacPorts/$MACPORT_FILE.tar.bz2
+tar xf $MACPORT_FILE.tar.bz2
+cd $MACPORT_FILE/
 ./configure
 make
 sudo make install
 cd ..
-port install glib binutils cmake openssl python
+rm $MACPORT_FILE.tar.bz2 $MACPORT_FILE
+port install glib binutils cmake openssl python mutt
 # Setup my shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Change the oh-my-zsh theme in ~/.zshrc (usually use agnoster)"
 sed 's/.*ZSH_THEME=.*/ZSH_THEME="agnoster"/' ~/.zshrc
 sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark
-chsh -s /bin/zsh $USER
+chsh -s /bin/zsh $USER  
 mkdir ~/Development
 mkdir ~/Config
 cd Config
